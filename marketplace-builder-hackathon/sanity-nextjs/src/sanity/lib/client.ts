4e296da039a22aa0b524aec8647587e988e00de0
KhawajaAbdulMoiz <khawajaahmedraza4@gmail.com>
2025-02-08 03:26:51 +0500

    Completed

--- marketplace-builder-hackathon/sanity-nextjs/src/sanity/lib/client.ts
+++ marketplace-builder-hackathon/sanity-nextjs/src/sanity/lib/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client';
 
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,       
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",       
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   token:process.env.SANITY_API_TOKEN || 'skLccAvpl4urQgcGq2gTutgbklAFxjEZBLYht2DR5oOVCv0lmLSDzOGgDriyZ5MscBsOIRTt5FQCHgjKK8IjHPMIdkfr3tCY4GoO33xhlgAvLKV6HgosJcrluXLfGbCjJff1iDS2nrDX2ZVCnY47xdHDONbiEiPjCGj3F9nChxsOwuTL4uXO',
   useCdn: false,  
